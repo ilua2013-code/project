22d9ae72fed5a0ae212e736b833df4e6d01d1b51
ilua2013 <ilua.2013@inbox.ru>
2026-02-07 18:43:21 +0300

    frontend: добавление интерфейса управления задачами

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-task-manager></app-task-manager>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'task-manager';
+}
---
+++ frontend/src/main.ts
@@ -0,0 +1,10 @@
+﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// Вариант 1: Абсолютный путь
+import { AppModule } from 'src/app/app.module';
+
+// Вариант 2: Или так (если не работает первый)
+// import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
